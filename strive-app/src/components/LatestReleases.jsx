15dcb116296b4a29292ba2a02eefa3c5fd587df3
hilina88 <hilinawuhib@gmail.com>
2021-12-14 15:28:33 +0100

    updated files

--- strive-app/src/components/LatestReleases.jsx
+++ strive-app/src/components/LatestReleases.jsx
@@ -1,15 +1,14 @@
-
 import React from "react";
 import { Card } from "react-bootstrap";
 import scifi from "../scifi.json";
 
-export const LatestReleases = () => {
+const LatestReleases = () => {
   return (
     <div className="bookwrap">
       {scifi.map((book) => {
         return (
-          <Card className="book-cover ">
-            <Card.Img className="card-image " variant="top" src={book.img} />
+          <Card className="book-cover">
+            <Card.Img className="card-image" variant="top" src={book.img} />
             <Card.Body>
               <Card.Title>{book.title}</Card.Title>
               <h5>{book.category}</h5>
