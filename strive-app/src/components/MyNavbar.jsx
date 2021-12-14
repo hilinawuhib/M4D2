60c28920813713754026fee5c52e7d1bd9b255de
hilina88 <hilinawuhib@gmail.com>
2021-12-14 17:45:37 +0100

    updated css

--- strive-app/src/components/MyNavbar.jsx
+++ strive-app/src/components/MyNavbar.jsx
@@ -6,7 +6,7 @@ const MyNavbar = () => (
         <Navbar.Brand href="#home">Strive Books</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
+            <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#about">About</Nav.Link>
                 <Nav.Link href="#browse">Browse</Nav.Link>
